6d6254f33075e67434c4689b4f026c47ca661c49
Naman Indranil <itscallednamanindranil@gmail.com>
2020-07-03 12:46:14 +0530

    first commit

---
+++ shinchan/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const routes = require('./routes.js');
+
+const path = require("path");
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',routes);
+app.post('/register',routes);
+app.get('/login',routes);
+app.post('/login',routes);
+app.get('/success',routes);
+
+//app.post('/addmsg',routes);
+app.post('/shortUrls',routes);
+app.get('/:shortUrl',routes);
+app.get('/logout',routes);
+app.listen(process.env.PORT || 3000 , ()=>console.log("SERVER STARTED"));
